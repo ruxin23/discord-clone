05313cf10f560eba52f9638e504c4747ef56ec21
longfeiwang <erwangfew@gmail.com>
2024-07-09 12:20:24 -0700

    init

---
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -0,0 +1,7 @@
+const ServerPage = () => {
+    return (
+        <>123123213</>
+    );
+}
+
+export default ServerPage;
---
+++ app/api/uploadthing/core.ts
@@ -0,0 +1,40 @@
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { UploadThingError } from "uploadthing/server";
+import { auth } from "@clerk/nextjs/server";
+const f = createUploadthing();
+
+const handleAuth = () => {
+    const userId = auth();
+    if (!userId) {
+        throw new UploadThingError("Unauthorized");
+    }
+    return { userId: userId };
+}
+
+
+// FileRouter for your app, can contain multiple FileRoutes
+export const ourFileRouter = {
+    // Define as many FileRoutes as you like, each with a unique routeSlug
+    serverImage: f({ image: { maxFileSize: "4MB", maxFileCount: 1 } })
+        // Set permissions and file types for this FileRoute
+        .middleware(() => handleAuth())
+        .onUploadComplete(() => { }),
+    messageFile: f(["image", "pdf"])
+        .middleware(() => handleAuth())
+        .onUploadComplete(() => { })
+    // This code runs on your server before upload
+    // If you throw, the user will not be able to upload
+    // Whatever is returned here is accessible in onUploadComplete as `metadata`
+
+    // .onUploadComplete(async ({ metadata, file }) => {
+    //     // This code RUNS ON YOUR SERVER after upload
+    //     console.log("Upload complete for userId:", metadata.userId);
+
+    //     console.log("file url", file.url);
+
+    //     // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
+    //     return { uploadedBy: metadata.userId };
+
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
